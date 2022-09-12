c7920c1020ebb802552fcb605d3907184c34cd14
bserrato7900 <bennyserrato5610@gmail.com>
2022-09-11 19:08:34 -0700

    communication between two python files

--- bot.py
+++ bot.py
@@ -1,22 +1,9 @@
 #!/usr/bin/python3.8
 import os
-from dotenv import load_dotenv
-from googletrans import Translator
+import discord
+import translator.py
 
 load_dotenv()
 
-# Translation Testing
-translator = Translator()
-test = []
-# Translation via lists
-translations = translator.translate(['The quick brown fox', 'jumps over', 'the lazy dog'], dest='ko')
-for translation in translations:
-    test.append(translation.text)
-    print(translation.origin, ' -> ', translation.text)
-print(test)
-# Testing translating back to original language
-translation_rev = translator.translate(test, dest='en')
-for translation in translation_rev:
-    print(translation.text)
-# Testing single string translation
-print(translator.translate('veritas lux mea', src='la').text)
+TOKEN = os.getenv("DISCORD TOKEN")
+
