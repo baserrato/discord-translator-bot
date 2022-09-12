11da89308f30d491c16344931a79d6eb5bbb83fd
bserrato7900 <bennyserrato5610@gmail.com>
2022-09-11 19:36:06 -0700

    feat: added discord connectivity

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python3.8
 import os
 import discord
-import translator.py
+#import translator
 
+from dotenv import load_dotenv
 load_dotenv()
 
-TOKEN = os.getenv("DISCORD TOKEN")
+TOKEN = os.getenv("DISCORD_TOKEN")
 
+client = discord.Client(intents=discord.Intents.default())
+
+@client.event
+async def on_ready():
+    print(f"{client.user} has connected to Discord!")
+
+client.run(TOKEN)
