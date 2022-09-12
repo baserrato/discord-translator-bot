16a50ebc6bea7a267546fa2e324ea811f0207414
Benjamin Serrato <54423854+bserrato7900@users.noreply.github.com>
2022-09-11 22:23:58 -0700

    Workflow fixes (#4)
    * fix: resolved some issue found with pylint
    
    * fix: resolved issues with imports failing for the workflow.
    
    * fix: changed versioning for python code
    
    * change: added fail threshold to CI pylint

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
-#!/usr/bin/python3.8
+#!/usr/bin/python3
+"""This script uses discord api to run a discord bot"""
 import os
 import discord
-import translator
 
 from dotenv import load_dotenv
 load_dotenv()
@@ -15,15 +15,16 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
+    """"prints out to confirm that discord bot is ready"""
     print(str(client.user) + " has connected to Discord!")
 
 @client.event
 async def on_message(message):
+    """Discord bot event handler to respond to certain triggers"""
     # to not event handle messages sent by the bot
     if message.author == client.user:
         return
 
     if "hello world" in message.content.lower():
         await message.channel.send("🌎 Hello World to you to!" + message.author.mention + "🌎")
-    
 client.run(TOKEN)
