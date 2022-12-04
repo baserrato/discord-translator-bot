f28ecf9a1d247e8cfa2e7f420334f62471d119b7
baserrato <bennyserrato5610@gmail.com>
2022-12-03 23:12:13 -0800

    feat: error handling for wrong command

--- bot.py
+++ bot.py
@@ -40,6 +40,7 @@ intents.message_content = True
 
 client = commands.Bot(intents=intents, command_prefix="!")
 translator = Translator()
+#removing default help for custom help command
 client.remove_command('help')
 
 @client.event
@@ -47,12 +48,31 @@ async def on_ready():
     """"prints out to confirm that discord bot is ready"""
     print(str(client.user) + " has connected to Discord!")
 
+@client.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.CommandNotFound):
+        error_command = re.compile("\".+\"").search(str(error)).group().replace("\"", "")
+        error_message = "Command `!" + error_command + "` Not Found.\n"
+        if error_command.lower() == "help":
+            error_message += "> Did you mean to use command: ```!help```"
+        elif error_command.lower() == "lang":
+            error_message += "> Did you mean to use command: ```!lang```"
+        elif error_command.lower() == "source":
+            error_message += "> Did you mean to use command: ```!source```"
+        elif error_command.lower() == "translate":
+            error_message += "> Did you mean to use command: ```!translate```"
+        elif error_command.lower() == "pronounce":
+            error_message += "> Did you mean to use command: ```!pronounce```"
+        error_message += "\nRefer to the command `!help` to see available commands."
+        await ctx.send(error_message)
+
 @client.command(name="help")
 async def help_command(ctx):
     help_string = "```Commands you can run with this bot: \n \n"
     help_string += "!lang -> shows a list of languages can be translated \n\n"
-    help_string += "!source `[phrase to translate]` -> gives language source of phrase. \n\n"
-    help_string += "!translate `[phrase to translate]` \"[language to translate to(optional)]\" \n\t\t-> translates phrase to chosen language (default is english)```"
+    help_string += "!source `[phrase to source]` -> gives language source of phrase. \n\n"
+    help_string += "!translate `[phrase to translate]` \"[language to translate to(optional)]\" \U0001F5E3(optional to include pronunciation recording)\n\t\t-> translates phrase to chosen language (default is english)\n\n"
+    help_string += "**!pronounce** `[phrase to pronounce]` ```"
     await ctx.send(help_string)
 
 @client.command(name='lang')
@@ -65,34 +85,45 @@ async def languages(ctx):
         elif idx < len(googletrans.LANGUAGES) - 1:
             lang_string += " | "
     lang_string += "```"
-    print("\U0001F5E3")
     await ctx.send(lang_string)
 
 @client.command(name='source')
 async def source(ctx):
-    wordRegex = re.compile("`[\w ]+`", re.UNICODE)
+    wordRegex = re.compile("`[\w\s]+`", re.UNICODE)
     result = wordRegex.search(ctx.message.content)
     if result != None:
         testPhrase = result.group()
         if testPhrase != None:
             phrase = testPhrase.replace("`", "")
             langSource = translator.detect(phrase).lang
-            await ctx.send(ctx.message.author.mention + " The phrase `" + phrase + "` is `" + googletrans.LANGUAGES[langSource] + "`")
+            await ctx.send(ctx.message.author.mention + " The phrase `" + phrase + "` is `" + googletrans.LANGUAGES[langSource.lower()] + "`")
     else:
         await ctx.send("Missing argument for `!source` requires phrase to source\nCommand Usage:```!source `[phrase]` ```")
 
 @client.command(name='translate')
 async def translate(ctx):
-    regexPhrase = re.compile("`[\w ]+`", re.UNICODE)
-    regexDest = re.compile("\"[A-Za-z]+\"")
+    regexPhrase = re.compile("`[\w\s]+`", re.UNICODE)
+    regexDest = re.compile("\".+\"")
     testPhrase = regexPhrase.search(ctx.message.content)
     testDest = regexDest.search(ctx.message.content)
     if testPhrase != None:
         phrase = testPhrase.group().replace("`", "")
         if testDest != None:
             destination = testDest.group().replace("\"", "")
-            translateResult = translator.translate(phrase, dest=destination)
-            await ctx.send(ctx.message.author.mention + " `" + phrase + "` ("+ googletrans.LANGUAGES[translateResult.src] + ") translates to `"   +  googletrans.LANGUAGES[translateResult.dest] + "` as: `" + translateResult.text + "`")
+            try:
+                translateResult = translator.translate(phrase, dest=destination)
+                await ctx.send(ctx.message.author.mention + " `" + phrase + "` ("+ googletrans.LANGUAGES[translateResult.src] + ") translates to `"   +  googletrans.LANGUAGES[translateResult.dest] + "` as: `" + translateResult.text + "`")
+            except:
+                error_message = "Language requested `"+ destination +"` not recognized"
+                if destination == "chinese":
+                    error_message += "\nDid you mean `chinese (simplified)[zh-cn]` or `chinese (traditional)[zh-tw]?`"
+                elif destination == "myanmar":
+                    error_message += "\nDid you mean `myanmar (burmese)`?"
+                elif destination == "kurdish":
+                    error_message += "\nDid you mean `kurdish (kurmanji)`?"
+                error_message += "\nRefer to !lang command to see all supported languages"
+                await ctx.send(error_message)
+                return
             emojiExtract = re.compile("\U0001F5E3")
             emojiFind = emojiExtract.search(ctx.message.content)
             if emojiFind != None:
@@ -101,7 +132,7 @@ async def translate(ctx):
                 await ctx.send(file=discord.File(r'dest.mp3'))
         else:
             translateResult = translator.translate(phrase)
-            await ctx.send(ctx.message.author.mention + " `" + phrase + "` ("+ googletrans.LANGUAGES[translateResult.src] + ") translates to `"   +  googletrans.LANGUAGES[translateResult.dest] + "` as: `" + translateResult.text + "`")
+            await ctx.send(ctx.message.author.mention + " `" + phrase + "` ["+ googletrans.LANGUAGES[translateResult.src.lower()] + "] translates to `"   +  googletrans.LANGUAGES[translateResult.dest.lower()] + "` as: `" + translateResult.text + "`")
             emojiExtract = re.compile("\U0001F5E3")
             emojiFind = emojiExtract.search(ctx.message.content)
             if emojiFind != None:
@@ -138,5 +169,4 @@ async def on_message(message):
         return
     #processes commands that are used in a message
     await client.process_commands(message)
-
 client.run(TOKEN)
